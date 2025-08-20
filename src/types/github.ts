72268e93532ed3bf7741496eba57ec206f77e3cf
MenaceHecker <thedynamic2508@gmail.com>
2025-08-20 15:19:55 -0400

    Added github interface

---
+++ src/types/github.ts
@@ -0,0 +1,11 @@
+export interface Repository {
+  id: number;
+  name: string;
+  full_name: string;
+  description?: string;
+  language?: string;
+  stargazers_count: number;
+  forks_count: number;
+  updated_at: string;
+  private: boolean;
+}
